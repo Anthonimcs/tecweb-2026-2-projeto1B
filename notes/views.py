cb2355f586d6ee65ec2a836522c68cb6d71c6a1b
anthoni-serro <anthonimcs@al.insper.edu.br>
2026-09-10 09:02:56 -0300

    Reimplementação do Projeto 1A usando Django ; OK

--- notes/views.py
+++ notes/views.py
@@ -14,3 +14,26 @@ def index(request):
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request, id):
+    note = Note.objects.get(id=id)
+    note.delete()
+    return redirect('index')
+
+def update(request, id):
+    note = Note.objects.get(id=id)
+    if request.method == 'POST':
+        title = request.POST.get('titulo').strip()
+        content = request.POST.get('detalhes').strip()
+        note.title = title
+        note.content = content
+        if not title or not content:
+            return render(request, 'notes/update.html', {
+                                "note": note,
+                                "error": "Preencha o título e o conteúdo da anotação."
+            })
+        else:
+            note.save()
+            return redirect('index')
+    else:
+        return render(request, 'notes/update.html', {'note': note})
